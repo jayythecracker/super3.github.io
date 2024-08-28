db832f1c114a294a20b3dfaa62678532c12544ef
J4ckkkkkk <jacckk2004@gmail.com>
2024-08-28 16:45:47 +0630

    may is playing volian

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"dcc063c8deae3e8ee46b78c1b20b572278474c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1262749326"
+    serviceWorkerVersion: "364158286"
   }
 });
